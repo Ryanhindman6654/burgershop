72cf10872437d4c8740279f9482c5adeb9ba4f19
tetlie <marius.tetlie@gmail.com>
2021-02-26 09:18:40 +0100

    register-user

---
+++ components/InputBlock/index.jsx
@@ -0,0 +1,26 @@
+import react from 'react'
+
+function InputBlock({
+  inputName,
+  inputId,
+  inputType,
+  inputPlaceholder,
+  labelText,
+  inputChangeHandler
+}) {
+
+  return (
+    <>
+      <label htmlFor={inputId}>{labelText}</label>
+      <input
+        type={inputType}
+        name={inputName}
+        id={inputId}
+        placeholder={inputPlaceholder}
+        onChange={event => inputChangeHandler(event)}
+      />
+    </>
+  )
+}
+
+export default InputBlock;
--- pages/user_create.js
+++ pages/user_create.js
@@ -1,25 +1,68 @@
 import firebaseInstance from '../config/firebase'
-import  { useEffect } from 'react'
+import  { useState, useEffect } from 'react'
+import InputBlock from '../components/InputBlock'
 
 function CreateUser() {
 
-  useEffect(() => {
-    const userCollection = firebaseInstance.firestore().collection('users');
+  const [userEmail, setUserEmail] = useState(null);
+  const [userPassword, setUserPassword] = useState(null);
 
+  function handleUserEmailChange(event) {
+    setUserEmail(event.target.value);
+  }
+
+  function handleUserPasswordChange(event) {
+    setUserPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(userEmail, userPassword)
+
+    const userCollection = firebaseInstance.firestore().collection('users');
     userCollection.doc().set({
-      email: 'mate0@gmail.com',
-      password: 'passordetmitt'
+      email: userEmail,
+      password: userPassword
     })
-    .then(() => {
-      console.log('Lagt til');
-    })
-    .catch(error => {
-      console.error(error);
-    })
-  }, [])
+      .then(() => {
+        console.log('Lagt til');
+        // state, en melding dukker opp om at det er lagt til
+        // brukeren blir sendt videre? =>
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
 
   return (
-    <h1>New user</h1>
+    <main>
+      <h1>New user</h1>
+      <form
+        name='add-user'
+        id='add-user'
+        action='/'
+        methon='GET'
+        onSubmit={event => handleSubmit(event)}
+      >
+        <InputBlock
+          inputName='email'
+          inputId='email'
+          inputType='email'
+          inputPlaceholder='Din epost'
+          labelText='E-post'
+          inputChangeHandler={event => handleUserEmailChange(event)}
+        />
+        <InputBlock
+          inputName='password'
+          inputId='password'
+          inputType='password'
+          inputPlaceholder='Ditt passord'
+          labelText='Passord'
+          inputChangeHandler={event => handleUserPasswordChange(event)}
+        />
+        <button type='submit'>Registrer</button>
+      </form>
+    </main>
   );
 };
 
