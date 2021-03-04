de299f65654fd8dfa16e6a2151791637ef6f8975
tetlie <marius.tetlie@gmail.com>
2021-03-04 10:42:28 +0100

    context, authentication

---
+++ config/auth.js
@@ -0,0 +1,50 @@
+import {createContext, useContext, useEffect, useState} from 'react'
+import firebaseInstance from './firebase';
+import nookies from 'nookies'
+
+const AuthContext = createContext({user: null});
+
+export function AuthProvider({children}) {
+
+  const [user, setUser] = useState()
+
+  useEffect(() =>{
+    return firebaseInstance.auth().onIdTokenChanged( async (user) => {
+      if(!user) {
+        setUser(null);
+        nookies.set(undefined, 'token', null, { path: '/' });
+      } else {
+        const token = user.getIdToken();
+        setUser(user);
+        nookies.set(undefined, 'token', token, { path: '/' });
+      }
+    });
+  });
+
+  // Sjekk hvert 10. minutt om en bruker er logget inn
+
+  useEffect(() => {
+    const handle = setInterval( async () => {
+      const user = firebaseInstance.auth().currentUser
+      if(user) await user.getIdToken(true)
+    }, 10 * 60 * 1000);
+
+    return clearInterval(handle);
+
+  });
+
+  // Logic
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
+
+/* 
+
+  Flytt state til noe globalt
+
+*/
--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import firebaseInstance from '../config/firebase'
 import { useRouter } from 'next/router'
+import {useAuth} from '../config/auth'
 
 const Profile = () => {
 
   const router = useRouter();
+  const userContext = useAuth();
+
+  console.log(userContext);
 
   const handleSignout = async () => {
     await firebaseInstance.auth().signOut();
